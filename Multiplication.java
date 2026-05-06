f44ef07cb4a1bc187951ac1cb31e5107662c5fdc
nagachandhan0609 <nagachandhan.0609@gmail.com>
2026-05-06 11:33:31 +0530

    coded to level3-1

--- Multiplication.java
+++ Multiplication.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Multiplication {
+public class Multiplication{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
